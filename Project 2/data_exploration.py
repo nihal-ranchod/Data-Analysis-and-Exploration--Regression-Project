5cf6a8ea3f0feed9c27ab909199be81b2a8c36f1
Lisa Godwin <lisa.godwin2002@gmail.com>
2024-05-15 12:12:11 +0200

    add: pairplot

--- Project 2/data_exploration.py
+++ Project 2/data_exploration.py
@@ -16,5 +16,16 @@ print(data.corr())
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.pairplot(data)
+# Customizing seaborn style
+sns.set(style="whitegrid")
+
+# Filter the dataset based on the "Stay" column
+stay_data = data[data['Stay'] == 1]  # Data for employees who stay
+leave_data = data[data['Stay'] == 0]  # Data for employees who leave
+
+combined_data = pd.concat([stay_data, leave_data])
+
+# Pair plot for both stay and leave groups
+sns.pairplot(combined_data.drop(columns=['ID']), hue='Stay', palette='husl')
+plt.suptitle('Pair Plot for Employees Who Stay(1) and Leave(0)', y=1)  # Title with some space above the plot
 plt.show()
